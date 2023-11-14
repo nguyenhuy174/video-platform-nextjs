f42fae66ca54784e90372ff62d70ef6caa73bfb4
Nguyen Ngoc Huy <huysuper1704@gmail.com>
2023-11-14 14:02:26 +0700

    final

--- components/FavoriteButton.tsx
+++ components/FavoriteButton.tsx
@@ -1,47 +1,15 @@
-import axios from 'axios';
-import React, { useCallback, useMemo } from 'react';
-import { PlusIcon, CheckIcon } from '@heroicons/react/24/outline';
-
-import useCurrentUser from '@/hooks/useCurrentUser';
-import useFavorites from '@/hooks/useFavorites';
+import { PlusIcon, CheckIcon } from "@heroicons/react/24/outline";
 
 interface FavoriteButtonProps {
-  movieId: string
+  movieId: string;
 }
 
-const FavoriteButton: React.FC<FavoriteButtonProps> = ({ movieId }) => {
-  const { mutate: mutateFavorites } = useFavorites();
-
-  const { data: currentUser, mutate } = useCurrentUser();
-
-  const isFavorite = useMemo(() => {
-    const list = currentUser?.favoriteIds || [];
-
-    return list.includes(movieId);
-  }, [currentUser, movieId]);
-
-  const toggleFavorites = useCallback(async () => {
-    let response;
-
-    if (isFavorite) {
-      response = await axios.delete('/api/favorite', { data: { movieId } });
-    } else {
-      response = await axios.post('/api/favorite', { movieId });
-    }
-
-    const updatedFavoriteIds = response?.data?.favoriteIds;
-
-    mutate({ 
-      ...currentUser, 
-      favoriteIds: updatedFavoriteIds,
-    });
-    mutateFavorites();
-  }, [movieId, isFavorite, currentUser, mutate, mutateFavorites]);
-  
-  const Icon = isFavorite ? CheckIcon : PlusIcon;
+const FavoriteButton: React.FC<FavoriteButtonProps> = () => {
+  const Icon = PlusIcon;
 
   return (
-    <div onClick={toggleFavorites} className="
+    <div
+      className="
     cursor-pointer 
     group/item 
     w-6 
@@ -55,10 +23,11 @@ const FavoriteButton: React.FC<FavoriteButtonProps> = ({ movieId }) => {
     justify-center 
     items-center 
     transition 
-    hover:border-neutral-300">
+    hover:border-neutral-300"
+    >
       <Icon className="text-white group-hover/item:text-neutral-300 w-4 lg:w-6" />
     </div>
-  )
-}
+  );
+};
 
 export default FavoriteButton;
--- pages/favorites.tsx
+++ pages/favorites.tsx
@@ -1,22 +1,16 @@
 import React from "react";
 import Navbar from "@/components/Navbar";
-import Billboard from "@/components/Billboard";
-import MovieList from "@/components/MovieList";
 import InfoModal from "@/components/InfoModal";
-import useMovieList from "@/hooks/useMovieList";
 import useInfoModal from "@/hooks/useInfoModal";
 
 const Home = () => {
-  const { data: movies = [] } = useMovieList();
   const { isOpen, closeModal } = useInfoModal();
 
   return (
     <>
       <InfoModal visible={isOpen} onClose={closeModal} />
       <Navbar />
-      <Billboard />
-      <div className="pb-40">
-        <MovieList title="Phim mới cập nhật" data={movies} />
+      <div className="pt-24">
       </div>
     </>
   );
